cc1ff2c5b39e738ac97c47c31791c8e8ba4e00af
Shubham076 <66319829+Shubham076@users.noreply.github.com>
2023-07-23 20:04:56 +0530

    Update arrowFunctions.js

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -7,6 +7,7 @@ in other words value of this is equal to value before calling the arrow function
 /*
 in this example closest lexical parent of inner is outer so value of this equals to obj
 itself
+The this value inside an arrow function is set to the this value of the enclosing context at the time the arrow function is created, regardless of how or where the arrow function is later invoked.
  */
 obj = {
     name: "Shubham",
